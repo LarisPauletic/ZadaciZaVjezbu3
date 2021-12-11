dfcebea317d16e512b6f9396bc52875ddfd839ad
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-12-11 16:41:37 +0100

    Add files via upload

---
+++ ZadaciZaVjezbu3/ZadaciZaVjezbu3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciZaVjezbu3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 7, 3, 8, 5, 9, 6, 2, 0, 4, 1 };
+
+            Sort x = new Sort();
+            x.QuickSort(arr, 1, arr.Length - 2);
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine(arr[i]);
+
+            Console.ReadKey();
+        }
+    }
+}
